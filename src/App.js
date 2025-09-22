5b6df911086bd8f060f1b28b815faa2672b01150
AxelLopMrtz <axel.82011@gmail.com>
2025-09-22 17:19:52 -0500

    boton modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor-app">
+      <div className="contenedor">
+        <h1 className="titulo">Sistema de Reserva de Transporte</h1>
+        <button className="boton-reserva" onClick={handleOpenModal}>
+          Reservar Transporte
+        </button>
+        {/* modal de prueba */}
+        {isModalOpen && (
+          <div className="modal" onClick={() => setIsModalOpen(false)}>
+            <div className="contenedor-modal" onClick={(e) => e.stopPropagation()}>
+              <p>Modal</p>
+              <button onClick={() => setIsModalOpen(false)}>Cerrar</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
